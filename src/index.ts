7c684863cf2117a12b7778f65a32d50063ac11fb
Jackson.Nguyen <huynvn97@gmail.com>
2024-11-19 00:56:52 +0700

    chore: export types

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import httpClient from './utils/httpClient';
 import {store as stateManagerStore} from './stateManager/store';
 
 export * from './modules/movie';
+export * from './modules/shared/types';
 
 export {
   httpClient, // Allow app to use or custom http client
