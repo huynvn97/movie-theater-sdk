97950f9a0b18492ba7450271b87e74da89657485
Jackson.Nguyen <huynvn97@gmail.com>
2024-11-19 16:09:39 +0700

    chore: allow app to init sdk

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,15 @@ import {store as stateManagerStore} from './stateManager/store';
 export * from './modules/movie';
 export * from './modules/shared/types';
 
+type InitOptions = {
+  baseURL: string;
+};
+function init(options: InitOptions) {
+  httpClient.defaults.baseURL = options.baseURL;
+}
+
 export {
+  init,
   httpClient, // Allow app to use or custom http client
   stateManagerStore,
 };
